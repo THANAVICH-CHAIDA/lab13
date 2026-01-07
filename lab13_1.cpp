79e4b25bb0f9a311624d9958264dd3563586b2e3
Thanavich <thanavich_c@cmu.ac.th>
2026-01-07 15:08:22 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,57 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+
+
+void stat(const double A[],int N,double B[]){
+    
+    double sum =0;
+    for(int i = 0; i < N ; i++){
+        sum += A[i];
+    }
+    double ArithmeticMean = sum / N;
+    B[0] = ArithmeticMean;
+    
+    
+    double sum2 = 0;
+    for(int i = 0; i < N ; i++){
+        
+        sum2 += pow((A[i] - ArithmeticMean ),2);
+    }
+    double secsum = sum2 / N;
+    double StandardDeviation = sqrt(secsum);
+    B[1] = StandardDeviation;
+    
+    
+    double sum3 = 1;
+    for(int i =0; i < N ; i++){
+        
+        sum3 *= A[i];
+    }
+    double GeometricMean = pow(sum3,1.0/N);
+    B[2] = GeometricMean;
+    
+    
+    double sum4 = 0;
+    for(int i =0 ; i < N ; i++){
+        
+        sum4 += 1.0/A[i];
+    }
+    double HarmonicMean = N / sum4;
+    B[3] = HarmonicMean;
+    
+        double MAX = A[0];
+        double MIN = A[0];
+    for(int i = 0 ; i<N ;i++){
+        
+        if (A[i] > MAX) MAX = A[i];
+        if (A[i] < MIN) MIN = A[i];
+        
+    }
+    
+    B[4] = MAX;
+    B[5] = MIN;
+     
+
+}
